c23f1ccc825ff2a68c3b491a2419416eb783e6ec
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-04-24 19:37:49 +1200

    Showing off naive pixel rendering approach

---
+++ GalacticPestControl/Assets/BackNForth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackNForth : MonoBehaviour {
+    private Vector3 _startPos;
+
+    // Use this for initialization
+    void Start () {
+        _startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = _startPos + Vector3.right * Mathf.Sin(Time.realtimeSinceStartup);
+	}
+}
