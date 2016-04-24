c9a691d0adec2b3ef4d3a71245514da75dc6d7e8
joelectronics <righteouslyalgebraic@gmail.com>
2016-04-25 11:43:56 +1200

    Added basic moveable player

---
+++ GalacticPestControl/Assets/Resources/Scripts/ExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    /// <summary>
+    /// An extension method by Joe. Takes a Rigidbody2D and moves it like Rigidbody.Move, but using force (so that the Rigidbody is not moved kinematically). 
+    /// </summary>
+    /// <param name="rb"></param>
+    /// <param name="targetVelocity"></param>
+    /// <param name="accelerationDuration"></param>
+    public static void ForceMove(this Rigidbody2D rb, Vector2 targetVelocity)
+    {
+        Vector3 Vf = targetVelocity;
+        Vector3 Vi = rb.velocity;
+
+        Vector3 F = (rb.mass * (Vf - Vi)) / 0.05f;      //0.05 is a good time. Feels just like Rigidbody.Move           
+        rb.AddForce(F, ForceMode2D.Force);
+    }
+
+    /// <summary>
+    /// Extension method by Joe. Returns the angle between this Vector and another Vector, assuming Z-axis as axis of rotation and Vector2.Up as 0 degrees (i.e, NORTH). Increases clockwise until 360 degrees.
+    /// </summary>
+    /// <param name="thisVector"></param>
+    /// <param name="otherVector"></param>
+    /// <returns></returns>
+    public static float Angle360To(this Vector3 thisVector, Vector3 otherVector)
+    {
+        return Vector3.Angle(thisVector, otherVector) * (Vector3.Cross(thisVector, otherVector).z > 0 ? -1f : 1f);
+    }
+}
+
---
+++ GalacticPestControl/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float MoveSpeed = 10f;
+
+    [Header("Weapon")]
+    [SerializeField] Transform WeaponHolder;
+    [SerializeField] float Radius = 0.5f;
+ 
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+
+        //Move player
+        Vector2 moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.ForceMove(moveDir * MoveSpeed);
+
+        //Rotate crosshair around player
+        Vector3 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        WeaponHolder.transform.position += mouseDelta;        
+
+        /*
+        Vector3 mouseDir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;        
+        WeaponHolder.transform.position = transform.position + (mouseDir * Radius);              
+        */
+    }
+}
