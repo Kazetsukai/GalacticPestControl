61f903572eb13657682fef6a7b4d8032d5538c17
joelectronics <righteouslyalgebraic@gmail.com>
2016-04-25 16:26:40 +1200

    Use xbox controller to rotate torch in Joe 3D scene

---
+++ GalacticPestControl/Assets/Resources/Scripts/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform, Camera.main.transform.up);
+
+    }
+}
--- GalacticPestControl/Assets/Resources/Scripts/Player.cs
+++ GalacticPestControl/Assets/Resources/Scripts/Player.cs
@@ -3,48 +3,38 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float MoveSpeed = 10f;
+    [SerializeField] float MoveSpeed = 3.5f;
 
     [Header("Weapon")]
+    [SerializeField] Transform WeaponPivot;
     [SerializeField] Transform WeaponHolder;
-    [SerializeField] float Radius = 0.5f;
- 
+    [SerializeField] float RotRate = 5f;
+    [SerializeField] float RotInputDeadzone = 0.4f;
 
     Rigidbody2D rb;
+    float aimAngle;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Cursor.lockState = CursorLockMode.Locked;
     }
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F1))
-        {
-            if (Cursor.lockState == CursorLockMode.Locked)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-            else
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-        }
 
         //Move player
-        Vector2 moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 moveDir = new Vector2(Input.GetAxis("L_XAxis_1"), Input.GetAxis("L_YAxis_1"));
         rb.ForceMove(moveDir * MoveSpeed);
 
         //Rotate crosshair around player
-        Vector3 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        WeaponHolder.transform.position += mouseDelta;        
+        Vector3 aimInput = new Vector2(Input.GetAxis("R_XAxis_1"), Input.GetAxis("R_YAxis_1"));
 
-        /*
-        Vector3 mouseDir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;        
-        WeaponHolder.transform.position = transform.position + (mouseDir * Radius);              
-        */
+        if (aimInput.magnitude >= RotInputDeadzone)
+        {
+            aimAngle = Vector3.up.Angle360To(aimInput.normalized) - 90f;      //Joe: I don't know why we need to subtract 90 degrees...
+        }
+        WeaponPivot.transform.rotation = Quaternion.Lerp(WeaponPivot.transform.rotation, Quaternion.Euler(new Vector3(WeaponPivot.transform.localEulerAngles.x, WeaponPivot.transform.localEulerAngles.y, aimAngle)), aimInput.magnitude * RotRate * Time.deltaTime);   //Multiply by aimInput.magnitude to get suuuper smooooth slow and fast rotation! Hurray! Could go even smoother if using an AnimationCurve multiplier too.  
+
+
     }
 }
