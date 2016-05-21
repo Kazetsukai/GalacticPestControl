2a0efbe24e0f9912444a20df9a9edfc96b3d0484
joelectronics <righteouslyalgebraic@gmail.com>
2016-05-21 17:33:53 +1200

    stuff

--- GalacticPestControl/Assets/Resources/Maps/ScienceLab_1.tsx
+++ GalacticPestControl/Assets/Resources/Maps/ScienceLab_1.tsx
@@ -1,25 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="ScienceLab_1" tilewidth="32" tileheight="32" tilecount="100" columns="10">
  <image source="../Textures/Tiles/ScienceLab_1/Tiles_ScienceLab_1l.png" width="320" height="320"/>
- <terraintypes>
-  <terrain name="wall" tile="-1"/>
- </terraintypes>
  <tile id="0">
   <properties>
    <property name="wall" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="2" terrain="0,0,0,0">
+ <tile id="2">
   <properties>
    <property name="wall" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="3" terrain="0,0,0,0">
+ <tile id="3">
   <properties>
    <property name="wall" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="4" terrain="0,0,0,0">
+ <tile id="4">
   <properties>
    <property name="wall" type="bool" value="true"/>
   </properties>
@@ -29,7 +26,7 @@
    <property name="wall" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="20" terrain="0,0,0,0">
+ <tile id="20">
   <properties>
    <property name="wall" type="bool" value="true"/>
   </properties>
